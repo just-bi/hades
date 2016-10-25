08e8bf40f8eae2c02076a92a4255831b17fbd070
rpbouman <roland.bouman@gmail.com>
2016-10-25 03:31:15 +0200

    Copyright notice: attribute Glenn Cheung

--- procedures/p_get_view_basecols.sql
+++ procedures/p_get_view_basecols.sql
@@ -1,5 +1,6 @@
 /**
 * Copyright 2016 Roland Bouman, Just-Bi.nl
+*                Glenn Cheung, Just-Bi.nl   
 * 
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
